f0d555e3ec18183c0b6a2294a5ff6c892981a20a
Yash <bajajy82@gmail.com>
2022-09-04 14:57:35 +0530

    increased cookies expiresin time

--- pages/api/auth/signin.js
+++ pages/api/auth/signin.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         id: existingStudent._id,
       },
       process.env.JWT_SECRET,
-      { expiresIn: "24h" }
+      { expiresIn: "23h" }
     );
     res.status(200).json({ user: existingStudent, token });
   } catch (err) {
